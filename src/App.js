bafde61c128039c94b3b3b32c99aca7884fa0910
yongyut.w <yongyut.w@cliniter.com>
2025-05-20 11:28:01 +0700

    edit text

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
+          CI/CD Test
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
